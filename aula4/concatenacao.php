9c44f913fdaec73eb44d839e11b040621dc87b50
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-12 15:40:20 -0300

    feat: finaliza aula sobre concatenação

---
+++ aula4/concatenacao.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concatenação</title>
+</head>
+<body>
+    <?php 
+    
+        // concatenção
+
+
+        $nome = "iago";
+
+        $cor = 'verde';
+
+        $idade = 20;
+
+
+        // para concatenar usasse o .
+
+        echo 'Olá' .$nome .', vi que sua cor favorita é' . $cor .', estou vendo também que você tem' . $idade .' anos e gosta de jogar bola';
+
+        // Usando aspas duplas para concatenar
+
+        echo "Olá $nome, vi que sua sua cor favoritaé $cor, estou vendo também que você tem $idade"
+
+    
+    ?>php
+</body>
+</html>
