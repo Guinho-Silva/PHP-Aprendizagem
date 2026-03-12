cbf5cac979d1bacf40c128d857c1e15c5a5d4d27
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-12 14:59:01 -0300

    feat: Aula sobre os blocos de tags php em html

---
+++ embutindo_blocos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <!-- Tag padrão -->
+    <?php 
+        echo 'Utilizando a tag padrão';
+    
+    ?>
+
+    <br>
+    <!-- Tag de impressão -->
+    <?= 'Utilizando a tag impressão' ?>
+
+    <? echo 'Utilizando a tag curta' ?>
+
+</body>
+</html>
