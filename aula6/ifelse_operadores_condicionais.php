e75f1145ba5db2192892134e3aa94b1b46e94aea
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-13 18:03:51 -0300

    feat: uso do if e operadores lógicos

---
+++ aula6/ifelse_operadores_condicionais.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+        $num = 2;
+
+        // == -> igual
+
+        // === <- verifica se os dois são iguais e do mesmo tipo
+
+        // != ou <> -> diferente
+
+        // !== -> não identico, verifica se são comparados e do mesmo tipo
+
+        // < -> verifica se algo é MENOR que outro algo
+
+        // > -> verifica se algo é MAIOR que outro algo
+         
+        // <= verifica se o valor é menor ou igual
+
+        // >= verifica se o valor é maior ou igual
+
+        if($num == 2) {
+            echo 'verdadeiro';
+        }
+        else{
+            echo 'falso';
+        }
+    
+    ?>
+</body>
+</html>
