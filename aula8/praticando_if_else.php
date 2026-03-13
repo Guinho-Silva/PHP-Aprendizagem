285f3bf070f9535505fa82b1ba1c065fad298f8e
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-13 20:11:59 -0300

    feat: praticando o uso dos operadores

---
+++ aula8/praticando_if_else.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+        // 
+        $usuario_possui_cartao = true;
+
+        $valor_compra = 225;
+
+        $valor_frete = 50;
+
+        $recebeu_desconto_frete = false;
+
+        if($usuario_possui_cartao == true AND $valor_compra >= 100){
+
+            $valor_frete = 0;
+            $recebeu_desconto_frete = true;
+        }
+    
+    ?>
+
+    <h1>Detalhes do pedido</h1>
+
+    <p>Possui cartão da loja?
+        
+        <?php       
+           if( $usuario_possui_cartao== true){
+            echo 'SIM';
+           }
+            else{
+            echo 'NÂO';
+            }
+        ?>
+    </p>
+
+    <p>Valor da compra: <?= $valor_compra ?></p>
+
+    <p>Recebeu desconto no frete?
+
+        <?php 
+            if($recebeu_desconto_frete == true){
+                echo 'SIM';
+            }
+
+            else{
+                echo 'NÃO';
+            }
+
+        ?>
+    </p>
+
+    <p>Valor do frete: <?= $valor_frete ?></p>
+</body>
+</html>
