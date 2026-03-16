73ff4ef0eca0314155c2254272f1a81be262b5ac
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-16 19:18:10 -0300

    feat: praticando operadores aritmeticos

---
+++ aula13/operadores_aritmeticos.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+        $num1 = 12;
+
+        $num2 = 4;
+
+        // Realizando SOMA
+
+        echo 'A soma entre $num e $num2 é: '.($num1 + $num2);
+
+        // Realizando SUBTRAÇÃO
+
+        echo '<br />';
+
+        echo 'A subtração entre $num e $num2 é: '.($num - $num2);
+
+        echo '<br />';
+        // Relizando a MULTIPLICAÇÃO
+        echo 'A multiplicação entre $num e $num2 é de: '.($num * $num2);
+
+        echo '<br />';
+        
+        // Realizando a DIVISÃO
+        echo 'A divisão entre $num e $num2 é de: '.($num / $num2);
+
+        echo '<br />';
+        
+        // Realizando o MODULO
+        echo 'O resto da divisão entre $num e $num2 é de: '.($num % $num2);
+        
+        
+    
+    ?>
+</body>
+</html>
