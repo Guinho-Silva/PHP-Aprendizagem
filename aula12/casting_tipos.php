001a3b35eb2d84a22ea57689937c9ef8d86aa6bd
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-16 18:46:33 -0300

    feat: termina aula de casting de tipos

---
+++ aula12/casting_tipos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+        //gettype() => retorna o tipo da variavel
+
+        $valor = 10;
+
+        echo gettype($valor); # int
+
+        // convertendo para float
+
+        $valor2 = (float)$valor; #float
+
+        // convertendo para string
+
+        $valor3 = (string)$valor; # string
+
+        // convertendo para inteiro
+
+        $valor4 = (int)$valor2 
+    
+    ?>
+</body>
+</html>
