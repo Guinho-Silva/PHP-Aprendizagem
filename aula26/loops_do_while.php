92d7a2ff00dc43995e3941d3a3c00ad0d20e7bbb
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-23 19:38:59 -0300

    feat: Do While

---
+++ aula26/loops_do_while.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $x = 10;
+        do{
+            echo 'X - $x';
+
+            $x++;
+            // continue -> pula uma etapa de iteração
+
+            // break -> para a execução
+        }while($x < 9);
+
+        // O do fará a iteração enquanto a condição for verdadeira
+
+        // exemplo: enquanto não for 5 eu não paro
+    
+    
+    ?>
+</body>
+</html>
