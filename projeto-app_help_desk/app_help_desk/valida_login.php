2cf797dc00afccb96ccad117558a5018cb8e98c5
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-24 19:44:46 -0300

    feat: Autenticando usuarios através de um array

--- projeto-app_help_desk/app_help_desk/valida_login.php
+++ projeto-app_help_desk/app_help_desk/valida_login.php
@@ -1,4 +1,34 @@
 <?php 
+    // variavel que verifica se a autenticação foi realizada
+
+    $usuario_autenticacdo = false;
+
+
+    // usuarios do sistema - Teste SEM banco
+
+    $usuario_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+    );
+
+
+    foreach($usuario_app as $user){
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+            $usuario_autenticacdo = true; //variavel de controle
+        }
+    }
+        
+    if($usuario_autenticacdo){
+        echo 'Usuário autenticado com sucesso!';
+    }
+    else{
+        # header('location: index.php') //funciona como se fosse um desvio caso não atenda um requisito
+        header('location: index.php?login=erro');
+    }
+        
+        
+
+
     // --USANDO O GET--
 
     /*$_GET['email']; // O get pega os "name" definidos no html
