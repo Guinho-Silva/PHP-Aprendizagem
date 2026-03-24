5b41c9beb1153afd5606e31fb5a44d7131eac321
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-24 17:41:01 -0300

    Realiza atividade de fixação de conteudo

---
+++ atividades/atividade_arrays_loops.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- rand() -->
+     <?php 
+ 
+        
+        for($i = 0; $i < 6; $i++){
+            $array= array(rand(1,60));
+        }
+        
+        print_r($array)
+     ?>
+</body>
+</html>
