92088727ebe65e052b7c8247d734428bf458e800
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-24 19:01:32 -0300

    feat: Adiciona projeto help desk de aprendizado

---
+++ projeto-app_help_desk/app_help_desk/valida_login.php
@@ -0,0 +1,18 @@
+<?php 
+    // --USANDO O GET--
+
+    /*$_GET['email']; // O get pega os "name" definidos no html
+    $_GET['senha']; // O método get expoem o paramentros passados para URL
+    */
+
+    // --USANDO O POST--
+
+    $_POST['email'];
+
+    $_POST['senha'];
+    // O método post anexa os dados feitos por um formulario na própria requisção
+
+    // Para atribuir o método post é so por no form `method= $_POST` 
+
+
+?>
