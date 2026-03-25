4c2dc36b61d5a3df39c6a2d2ef5a894c30c7516f
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-25 19:01:25 -0300

    feat: adiciona o require_once

--- projeto-app_help_desk/app_help_desk/abrir_chamado.php
+++ projeto-app_help_desk/app_help_desk/abrir_chamado.php
@@ -1,13 +1,4 @@
-<?php 
-  session_start();
-  
-  if(!isset($_SESSION['Autenticado']) OR $_SESSION['Autenticado'] != 'SIM'){
-    header('location: index.php?login=erro2');
-  }
-
-  echo $_SESSION['Autenticado'];
-
-?>
+<? require_once("validador_acesso.php")?>
 
 <html>
   <head>
