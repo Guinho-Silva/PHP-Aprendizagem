e6d404c310958348ca7a7d878e980e8289118bb8
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-27 17:40:05 -0300

    feat: cria o egistra_chamado

---
+++ projeto-app_help_desk/app_help_desk/registra_chamado.php
@@ -0,0 +1,37 @@
+<?php 
+
+    echo '<pre>';
+
+    print_r($_POST);
+
+    echo '</pre>';
+
+    // Lembre-se
+
+    // Atribue names aos input de caixa de valor
+
+    $_arquivo = fopen('arquivo.txt','a');
+
+
+    $titulo = str_replace('#', '-', $_POST['titulo']);
+
+    $categoria = str_replace('#', '-', $_POST['categoria']);
+
+    $descricao = str_replace('#', '-', $_POST['descricao']);
+
+    //Tentar usar o implode('#', $_POST)
+
+    $texto = $titulo . '#' . $categoria . '#' . $descricao;
+
+    // PHP_EOL -> concatenar com o texto
+    
+    fwrite($_arquivo, $texto);
+
+    fclose($_arquivo);
+
+    header('Location: abrir_chamado.php');
+
+    echo $texto;
+
+
+?>
