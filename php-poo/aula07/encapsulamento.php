bc5dc0e8ef78a5d0cc640f8468ffbb8f626728c1
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-04-13 18:24:12 -0300

    feat: Public, Private, Protected. Entendo seus uso

---
+++ php-poo/aula07/encapsulamento.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        class Pai{
+            private $nome = 'Iago';
+            protected $sobrenome = 'Silva';
+            public $humor = 'Feliz';
+
+    
+        }
+
+        $pai = new Pai();
+
+        echo $pai
+     
+    
+    
+    ?>
+</body>
+</html>
